5c374d7e8f9cebc9447fc16499b3aad7bc29f125
SamanthaNeilen <33734383+SamanthaNeilen@users.noreply.github.com>
2020-06-29 10:24:23 +0200

    Api test tools

---
+++ NetCore.WebApiExample/Controllers/ValuesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace NetCore.WebApiExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new [] {"value1", "value2"});
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok($"Requested id: {id}");
+        }
+
+        [HttpPost]
+        public IActionResult PostObjectParameter([FromBody] KeyValuePair<int,string> keyValuePair)
+        {
+            return Ok($"Posted values {keyValuePair}");
+        }
+        
+        [HttpPost("{id}")]
+        public IActionResult PostValueTypeParameter([FromRoute]int id, [FromBody] KeyValuePair<string,string> value)
+        {
+            return Ok($"Posted values {{ id: {id}, value: {value} }}");
+        }
+    }
+}
